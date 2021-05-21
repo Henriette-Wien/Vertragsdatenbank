8a9b5cc7c1453cc334783723a6fb3a0d954568a4
Henriette Wien <h.s.wien@gmx.de>
2021-05-21 10:42:15 +0200

    first commit

---
+++ src/main/java/com/example/vertragsdatenbank/VertragsdatenbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.vertragsdatenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertragsdatenbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertragsdatenbankApplication.class, args);
+    }
+
+}
