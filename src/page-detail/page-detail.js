0b228e645aa2183e9dacae9b1e474ebd02a8b933
Henriette Wien <h.s.wien@gmx.de>
2021-06-07 16:26:47 +0200

    JS und CSS Page

---
+++ src/page-detail/page-detail.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/**
+ * Klasse PageDetail: Stellt die Detailseite der App zur Verfügung
+ */
+class PageDetail {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+        this._recordVertrag = -1;
+        this._data = null;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show(matches) {
+        // URL-Parameter auswerten
+        this._recordVertrag = matches[1];
+        this._data = await this._app.database.selectAllContracts();
+
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("page-detail/page-detail.html");
+        let css = await fetch("page-detail/page-detail.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+        await this._renderPosts(pageDom);
+
+        this._app.setPageTitle(`Vertrag ${this._recordVertrag}`, {isSubPage: true});
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+
+    /**
+     * Hilfsmethode, welche den HTML-Code der eingelesenen HTML-Datei bearbeitet
+     * und anhand der eingelesenen Daten ergänzt. Zusätzlich wird hier ein
+     * Event Handler für den Button registriert.
+     *
+     * @param {HTMLElement} pageDom Wurzelelement der eingelesenen HTML-Datei
+     * mit den HTML-Templates dieser Seite.
+     */
+
+    async _renderPosts(pageDom) {
+        let mainElement = pageDom.querySelector("main");
+        let countryTemplateElement = pageDom.querySelector("#template-post");
+
+        this._data.forEach(post => {
+            if (this._recordVertrag === post.land) {
+                let html = countryTemplateElement.innerHTML;
+                html = html.replace("{YEAR}", post.year);
+                html = html.replace("{TITLE}", post.title);
+                html = html.replace("{AUTHOR}", post.author);
+                html = html.replace("{CONTENT}", post.content);
+                if (post.img !== undefined && post.img !== "") {
+                    let imgTemplateElement = pageDom.querySelector("#template-img");
+                    let imgHTML = imgTemplateElement.innerHTML.replace("{SRC}", post.img);
+                    html = html.replace("{IMG}", imgHTML);
+                } else {
+                    html = html.replace("{IMG}", "");
+                }
+                mainElement.innerHTML += html;
+            }
+        });
+    }
+}
---
+++ src/page-overview/page-overview.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/**
+ * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
+ */
+class PageOverview {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show() {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("page-overview/page-overview.html");
+        let css = await fetch("page-overview/page-overview.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+        await this._renderCountries(pageDom);
+
+        this._app.setPageTitle("Startseite");
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+
+    /**
+     * Darstellung der Länder  auf der Startseite erzeugen.
+     *
+     * @param {HTMLElement} pageDom Wurzelelement der eingelesenen HTML-Datei
+     * mit den HTML-Templates dieser Seite.
+     */
+    async _renderCountries(pageDom) {
+        let mainElement = pageDom.querySelector("main");
+        let countryTemplateElement = pageDom.querySelector("#template-country");
+
+        let countries = await this._app.database.selectAllCountries();
+
+        countries.forEach(country => {
+            let html = countryTemplateElement.innerHTML;
+            html = html.replace("{ID}", country.id);
+            html = html.replace("{NAME}", country.name);
+            html = html.replace("{IMG}", country.img);
+            html = html.replace("#", "#/Country/" + country.id);
+
+            mainElement.innerHTML += html;
+        });
+    }
+}
