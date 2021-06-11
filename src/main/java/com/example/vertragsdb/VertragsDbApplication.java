11c0f2c0aea36c9ef9dba94f8130004e8f5a9cbe
Henriette Wien <h.s.wien@gmx.de>
2021-06-11 11:30:26 +0200

    Project Java

---
+++ src/main/java/com/example/vertragsdb/VertragsDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.vertragsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertragsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertragsDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vertragsdb/VertragsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vertragsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertragsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
