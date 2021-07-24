89b4605dca5bf03492ab2563d242bf13da597433
Henriette Wien <h.s.wien@gmx.de>
2021-07-24 09:57:16 +0200

    Integration Test

--- src/test/java/com/example/vertragsdb/connectionhelper/MongoDbSpringIntegrationTest.java
+++ src/test/java/com/example/vertragsdb/connectionhelper/MongoDbSpringIntegrationTest.java
@@ -1,7 +1,11 @@
 package com.example.vertragsdb.connectionhelper;
 
+import com.example.vertragsdb.model.Vertrag;
+import com.example.vertragsdb.repository.VertragRepository;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +13,26 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 public class MongoDbSpringIntegrationTest {
-    /*@DisplayName("given object to save"
-            + " when save object using MongoDB template"
-            + " then object is saved")
-     */
+
+    @Autowired
+    VertragRepository vertragRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        vertragRepository.save(new Vertrag());
+    }
+
+    @Test
+    public void shouldBeNotEmpty() {
+        assertThat(vertragRepository.findAll()).isNotEmpty();
+    }
+
+
     @Test
     public void test(@Autowired MongoTemplate mongoTemplate) {
         // given
@@ -27,9 +44,9 @@ public class MongoDbSpringIntegrationTest {
         mongoTemplate.save(objectToSave, "collection");
 
         // then
-       /* Assertions.assertThat(mongoTemplate.findAll(DBObject.class, "collection")).extracting("key")
+        Assertions.assertThat(mongoTemplate.findAll(DBObject.class, "collection")).extracting("key")
                 .containsOnly("value");
 
-        */
+
     }
 }
